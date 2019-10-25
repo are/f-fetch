aee25866070aa98885feb7f64518b00ae91f361d
Artur Wojciechowski <artur.wojciechowski@auto1.com>
2019-10-25 13:30:21 +0200

    :bug: Fix target

--- src/ops/target.js
+++ src/ops/target.js
@@ -12,12 +12,12 @@ const METHODS = [
     'PATCH',
 ]
 
-export const (_, target) = (method, ...url) => {
+export const target = (method, ...url) => {
     assert.ok(METHODS.includes(method), `Incorrect method '${method}'.`)
     assert.ok(url !== undefined, `URL cannot be undefined.`)
     assert.ok(url !== '', `URL cannot be empty.`)
 
-    return request => {
+    return (_, request) => {
         request.on('before', req => ({
             ...req,
             url: url.map(e => `${e}`).join('/'),
